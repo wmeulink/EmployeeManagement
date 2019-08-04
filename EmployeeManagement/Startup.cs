acb19a100526555f03b523b6221ec486ad12f899
Whitney <wmeulink@gmail.com>
2019-08-04 12:31:54 -0700

    in process hosting

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -27,7 +27,7 @@ namespace EmployeeManagement
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync(System.Diagnostics.Process.GetCurrentProcess().ProcessName);
             });
         }
     }
